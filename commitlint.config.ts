4916807ff1140f161f8a5cb1511eaea20adc96b6
3raphat <treeraphat234@gmail.com>
2024-11-06 03:23:23 +0700

    chore: update commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,28 @@
-export default {
-  parserPreset: "conventional-changelog-conventionalcommits",
+import type { UserConfig } from "@commitlint/types"
+
+const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-conventionalcommits",
+  formatter: "@commitlint/format",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 }
+
+export default config
