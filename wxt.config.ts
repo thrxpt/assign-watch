05a2f86e8502f1750b4bd1515b49125b5df83176
3raphat <96657413+3raphat@users.noreply.github.com>
2025-02-04 22:27:02 +0700

    feat: add Firefox-specific settings to manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -21,6 +21,14 @@ export default defineConfig({
       if (wxt.config.mode === "development") {
         manifest.name += " (DEV)"
       }
+
+      if (wxt.config.browser === "firefox") {
+        manifest.browser_specific_settings = {
+          gecko: {
+            id: "assignwatch@leb2",
+          },
+        }
+      }
     },
   },
 })
