b8bcee9e17a9e397bb676b2363996605cb014a82
Theeraphat Jaingam <jain.theera@gmail.com>
2025-11-23 20:03:16 +0700

    feat: update popup UI

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import App from "@/entrypoints/popup/App";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-
-import "./style.css";
+import "@/assets/tailwind.css";
+import "@fontsource-variable/anuphan";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
